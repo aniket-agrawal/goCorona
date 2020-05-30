b5e9181f532f8634388acb8d30aa45274810edaa
aniket <aniket9636541817@gmail.com>
2020-05-30 23:22:53 +0530

    start1

--- app/src/main/java/com/example/login/Main3Activity.java
+++ app/src/main/java/com/example/login/Main3Activity.java
@@ -2,13 +2,26 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Main3Activity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void back(View view) {
+        mAuth.signOut();
+        startActivity(new Intent(Main3Activity.this,MainActivity.class));
+
     }
 }
