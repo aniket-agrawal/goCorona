65caa012dce833b0fac8f17ec16267ca7503e7be
Soumil <soumilsarawgi@gmail.com>
2020-06-01 14:09:26 +0530

    feedback update

---
+++ app/src/main/java/com/example/login/FeedBackFormActivity.java
@@ -0,0 +1,186 @@
+package com.example.login;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class FeedBackFormActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private String typeOfFeedBack="";
+    private EditText customFeedback;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private String safetyType;
+    private EditText feedbackText;
+
+    private String currentUserId;
+    private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
+
+    private String feedBackTextString="";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_back_form);
+
+
+        mAuth=FirebaseAuth.getInstance();
+        currentUserId=mAuth.getCurrentUser().getUid();
+        RootRef= FirebaseDatabase.getInstance().getReference();
+
+
+
+        feedbackText = findViewById(R.id.feedback_text);
+
+        spinner = (Spinner) findViewById(R.id.spinner_type_of_feedback);
+        customFeedback = findViewById(R.id.custom_feedback_type);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.typeoffeedback));
+
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(myAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+
+
+                if(position==1)
+                {
+                        typeOfFeedBack = "Reporting a nearby corona case";
+                }
+
+                if(position==2)
+                {
+                        typeOfFeedBack = "Need Govt. support";
+                }
+
+                if(position==3)
+                {
+                    typeOfFeedBack = "Queries Regarding Lockdown";
+                }
+
+                if(position==4)
+                {
+                    typeOfFeedBack = "Lack of Medical Facilities";
+                }
+
+                if(position==5)
+                {
+                    customFeedback.setVisibility(View.VISIBLE);
+                }
+
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(FeedBackFormActivity.this, "Please select the type", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        if(typeOfFeedBack.equals(""))
+        {
+            typeOfFeedBack = customFeedback.getText().toString();
+        }
+    }
+
+    public void checkbox(View view)
+    {
+        int radioId = radioGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(radioId);
+
+        safetyType = radioButton.getText().toString();
+
+    }
+
+    public void submitFeedback(View view)
+    {
+        UpdateSettings();
+    }
+
+    private void UpdateSettings()
+    {
+        feedBackTextString = feedbackText.getText().toString();
+
+        if(TextUtils.isEmpty(feedBackTextString))
+        {
+            Toast.makeText(this, "Please write a feedback", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(TextUtils.isEmpty(safetyType))
+        {
+            Toast.makeText(this, "Please let us know if you are safe", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(TextUtils.isEmpty(typeOfFeedBack))
+        {
+            Toast.makeText(this, "Please mention a feedback type", Toast.LENGTH_SHORT).show();
+        }
+        else
+            {
+                HashMap<String, Object> feedbackMap = new HashMap<>();
+                feedbackMap.put("uid", currentUserId);
+                feedbackMap.put("safety type", safetyType);
+                feedbackMap.put("feedback category", typeOfFeedBack);
+                feedbackMap.put("user feedback", feedBackTextString);
+
+
+                RootRef.child("Users").child(currentUserId).updateChildren(feedbackMap)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task)
+                            {
+                                if(task.isSuccessful())
+                                {
+                                    SendUserToMainPage();
+                                }
+
+                                else
+                                    {
+                                        String message = task.getException().toString();
+                                        Toast.makeText(FeedBackFormActivity.this, "Error: "+message, Toast.LENGTH_SHORT).show();
+                                    }
+                            }
+                        });
+
+
+            }
+
+
+
+
+    }
+
+    private void SendUserToMainPage()
+    {
+        Intent mainIntent = new Intent(FeedBackFormActivity.this,Mainpage.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+}
--- app/src/main/java/com/example/login/ui/feedback/FeedbackFormViewModel.java
+++ app/src/main/java/com/example/login/ui/feedback/FeedbackFormViewModel.java
@@ -1,16 +1,16 @@
-package com.example.login.ui.slideshow;
+package com.example.login.ui.feedback;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class FeedbackFormViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public FeedbackFormViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+        mText.setValue("This is feedback fragment");
     }
 
     public LiveData<String> getText() {
