adbe52c1dfb927660c190346223781a059b5c661
Soumil <soumilsarawgi@gmail.com>
2020-06-24 18:33:53 +0530

    Firebase

--- app/src/main/java/com/example/login/Constants.java
+++ app/src/main/java/com/example/login/Constants.java
@@ -2,10 +2,11 @@ package com.example.login;
 
 public class Constants {
 
+
     private static final String PACKAGE_NAME = "com.example.login";
-    static final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY";
-    static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
-    static final String LOCATION_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_DATA_EXTRA";
-    static final int SUCCESS_RESULT = 1;
-    static final int FAILURE_RESULT = 0;
+    public static final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY";
+    public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+   public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_DATA_EXTRA";
+   public static final int SUCCESS_RESULT = 1;
+   public static final int FAILURE_RESULT = 0;
 }
